67028bc1d00bd239e873cc5d74d3437e23f785d3
ceylinbrooks <ceylinbrooks@gmail.com>
2021-03-31 21:26:01 -0400

    Code/Test passing

---
+++ app/src/main/java/quotes/Quote.java
@@ -0,0 +1,17 @@
+package quotes;
+
+
+
+public class Quote {
+    String author;
+    String text;
+
+    public Quote(String author, String text) {
+        this.author = author;
+        this.text = text;
+
+    }
+}
+
+
+// A claas needs a toString method to print something other than the memory location
